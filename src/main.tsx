68410117ddaa0b5574403a4abb077c983372515c
entrepreneur000 <entrepreneurlifes0@gmail.com>
2025-10-17 18:23:28 +0400

    Import HashRouter in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,8 @@
-// src/main.tsx
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { HashRouter } from "react-router-dom";
 import "./index.css";
+import { HashRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
